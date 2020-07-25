d2faef7374bc93d639d3457f940bcfd5432ef620
Jurriaan Barkey Wolf <jjhbarkeywolf@gmail.com>
2020-07-25 13:49:26 +0200

    added 'dataset' definition for smoke test

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ parser.add_argument('--model_type', type=str, choices=['clam_sb', 'clam_mb', 'mi
 parser.add_argument('--exp_code', type=str, help='experiment code for saving results')
 parser.add_argument('--weighted_sample', action='store_true', default=False, help='enable weighted sampling')
 parser.add_argument('--model_size', type=str, choices=['small', 'big'], default='small', help='size of model, does not affect mil')
-parser.add_argument('--task', type=str, choices=['camelyon_40x_cv',  'tcga_kidney_cv'])
+parser.add_argument('--task', type=str, choices=['camelyon_40x_cv',  'tcga_kidney_cv', 'smoke_test'])
 ### CLAM specific options
 parser.add_argument('--no_inst_cluster', action='store_true', default=False,
                      help='disable instance-level clustering')
@@ -174,6 +174,21 @@ elif args.task == 'tcga_kidney_cv':
 
     if 'clam' in args.model_type:
         assert args.subtyping
+
+elif args.task == 'smoke_test':
+    args.n_classes=2
+    dataset = Generic_MIL_Dataset(csv_path = './smoke_test_labels.csv',
+                            data_dir= args.data_root_dir,
+                            shuffle = False,
+                            seed = args.seed, 
+                            print_info = True,
+                            label_dict = {'A':0, 'B':1},
+                            patient_strat= False,
+                            ignore=[])
+
+    # We've prepped .h5, and disabled .pt generation to save space
+    dataset.load_from_h5(True)
+    
 
 else:
     raise NotImplementedError
